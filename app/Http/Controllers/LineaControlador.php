1fba474c0de8c688835c62dbcf9707237df4804e
Oscar <odmarulandab@unal.edu.co>
2025-03-26 00:15:10 -0500

    initial commit: laravel API for pizzeria prject

---
+++ app/Http/Controllers/LineaControlador.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\reservaModelo;
+use App\Models\lineaModelo;
+use App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LineaControlador extends Controller
+{
+
+    public function getByPedido($idPedido) {
+        $lineas = lineaModelo::where('idPedido', $idPedido)->get();
+    
+        if ($lineas->isEmpty()) {
+            return response()->json([
+                'message' => 'No hay datos para el idPedido proporcionado'
+            ], 404);
+        }
+    
+        return response()->json($lineas, 200);
+    }
+    
+    public function index(){
+        $linea = lineaModelo::all();
+        if($linea->isEmpty()){
+            $data=[
+                'message' => 'no hay datos',
+            ];
+            return response()->json($data, 404);
+        }
+        return response()->json($linea,200);
+    }
+
+    public function store(Request $request){
+        $validacion = Validator::make($request->all(),
+        [
+            'idSabor' => 'Required',
+            'NumeroPorciones'=>'Required|numeric',
+        ]);
+        if($validacion->fails()){
+            $data=[
+                'message'=>'Error en la validacion de datos',
+                'errors'=>$validacion->errors(),
+                'status'=>200
+            ];
+            return response()->json($data,400);
+        }
+        $linea = lineaModelo::create(
+            [
+                'idPedido'=>reservaModelo::max('idPedido'),
+                'idSabor' => $request->idSabor,
+                'NumeroPorciones' => $request->NumeroPorciones,
+            ]
+        );
+
+        if(!$linea){
+            $data=[
+                'message'=>'Error al registrar linea',
+                'status'=>500
+            ];
+            return response()->json($data,500);
+        }
+        $data=[
+            'linea'=>$linea,
+            'status'=>201
+        ];
+        return response()->json($data,201);
+    }
+}
---
+++ app/Models/reservaModelo.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class reservaModelo extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'reserva';
+    public $timestamps = true;
+    protected $primaryKey = 'idPedido';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $fillable = [
+        'idPedido',
+        'Entregada',
+        'FechaHoraEntrega',
+        'PrecioTotal',
+        'UsuarioDocumento'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($reserva) {
+            $reserva->created_at = Carbon::now()->timezone('America/Bogota'); // Set timezone
+        });
+
+        static::updating(function ($reserva) {
+            $reserva->updated_at = Carbon::now()->timezone('America/Bogota'); // Set timezone
+        });
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(usuarioModelo::class, 'UsuarioDocumento', 'UsuarioDocumento');
+    }
+
+    public function lineas()
+    {
+        return $this->hasMany(lineaModelo::class, 'idPedido', 'idPedido');
+    }
+
+}
